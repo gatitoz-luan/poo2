10f4de80edd7775f74f071719b850efb77533a98
Paulo Eller Behr <87311523+Pebpaulo@users.noreply.github.com>
2022-04-26 19:41:30 -0300

    Update mercado.py

--- mercado.py
+++ mercado.py
@@ -4,8 +4,8 @@ class mercado():
         return produtos
 
     def ver_produtos(produtos):
-        print(produtos) 
-    
+        print(produtos)
+        
     def inserir(produtos):
         while True:
             nome = input('produto: ')
@@ -16,7 +16,6 @@ class mercado():
             continuar= input('continuar: S/N ')
             if 'N' in continuar.upper():
                 break
-        
     def deletar():
         while True:
             nome = input('produto: ')
@@ -24,40 +23,9 @@ class mercado():
                 del produtos[nome]
             print(nome,' deletado')
             print(produtos)   #teste
+            continuar= input('continuar: S/N ')
             if 'N' in continuar.upper():
                     break
-
-
-    def abertos(horario):
-        abertos=0
-        ana=(8,12)
-        if ana[0]<horario<ana[1]:
-            abertos+=1
-        joao=(9,15)
-        if joao[0]<horario<joao[1]:
-            abertos+=1
-        jose=(11,18)
-        if jose[0]<horario<jose[1]:
-            abertos+=1
-        alberto=(15,17)
-        if alberto[0]<horario<alberto[1]:
-            abertos+=1
-        luis=(16,20)
-        if luis[0]<horario<luis[1]:
-            abertos+=1
-        tadeu=(17,20)
-        if tadeu[0]<horario<tadeu[1]:
-            abertos+=1
-        return abertos
-    
-    
-horario=14  #importar lib horario atual
-#print("Neste horário há", abertos(horario) ,"caixas abertos")
-
-
-
-#mercado.deletar() 
-#mercado.inserir()
 
 while True:
     produtos= mercado.produtos()
@@ -78,8 +46,3 @@ while True:
         mercado.deletar()
     else:
         print("///////////////////opção inválida///////////////////")
-
-
-    
-     
-    
